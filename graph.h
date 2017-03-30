87714cc74c765ee2eabed86ef92366df1d4ed81d
Praveen Seeniraj <praveens1997@gmail.com>
2017-03-30 18:35:32 -0400

    Creating Graph algorithms

---
+++ graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct AdjListNode
+{
+    int adjecent;
+    struct AdjListNode* next;
+}AdjListNode;
+
+typedef struct AdjList
+{
+    struct AdjListNode* head; 
+}AdjList;
+
+typedef struct Graph
+{
+    int vertex;
+    struct AdjList* array;
+}Graph;
+
+AdjListNode* newAdjListNode(int dest);
+Graph* createGraph(int V);
+void insertEdge(Graph* graph, int src, int dest);
+void printGraph(Graph* graph);
+#endif
